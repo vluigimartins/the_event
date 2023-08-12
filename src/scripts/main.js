36590f5cfad4c8f297e3239c0e31786b78f7b83f
Vinicius <vini-luiz@hotmail.com>
2023-08-12 19:03:14 -0300

    finalizando o projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+const dataDoEvento = new Date("Oct 28, 2023 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+
+    if(distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+}, 1000);
